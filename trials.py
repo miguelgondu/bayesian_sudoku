fbe5cf1dc59e68bb6e4f65776f1e34493d3cc16b
Miguel Gonzalez Duque <migd@itu.dk>
2020-08-19 14:55:06 +0200

    fixes table name

--- trials.py
+++ trials.py
@@ -9,7 +9,7 @@ class Trials:
         """
         Takes a db connection.
         """
-        self.table_name = "trialsinfo"
+        self.table_name = "trials"
         self.db = db
 
     def execute_query(self, query):
